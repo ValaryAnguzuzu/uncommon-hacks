9b63be0e98676c1bd0d423757b4e091c0857e265
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2026-05-16 18:37:15 -0500

    added playerstate global to track all player data

--- scripts/PlayerState.gd
+++ scripts/PlayerState.gd
@@ -17,3 +17,41 @@ extends Node
 #
 # Other managers should read from and update PlayerState instead of keeping
 # separate copies of these values.
+extends Node
+
+# Money
+@export var checking_balance: int = 500
+@export var debt: int = 1200
+
+# Run Progress
+@export var week_num: int = 1
+@export var max_weeks: int = 15
+
+# Mental State
+@export_range(0, 100) var burnout: int = 10
+@export_range(0, 100) var confidence: int = 50
+
+# Career progress
+@export_range(0, 100) var interview_skill: int = 20
+@export var score: int = 0
+@export var applications_sent: int = 0
+
+# Info about the player that is updated throughout the game
+var resume_keywords: Array[String] = ["Git"]
+var completed_projects: Array[String] = []
+var active_interviews: Array[String] = []
+
+# helper functions for other scripts to modify unexported stuff
+func add_keyword(keyword: String) -> void:
+	if keyword not in resume_keywords:
+			resume_keywords.append(keyword)
+
+
+func complete_project(project_id: String) -> void:
+	if project_id not in completed_projects:
+			completed_projects.append(project_id)
+
+
+func add_active_interview(job_id: String) -> void:
+	if job_id not in active_interviews:
+			active_interviews.append(job_id)
